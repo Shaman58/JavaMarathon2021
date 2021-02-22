bcaa987d2304e839f4e5670f2835052352873065
A.Shmonin@outlook.com <A.Shmonin@outlook.com>
2021-02-22 21:35:00 +0300

    Add Day1 Tasks 1-6 solutions

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        final int k = 3;
+        for (int i = 1; i < 10; i++) {
+            System.out.printf("%d x %d = %d", i, k, (i * k));
+            System.out.println();
+        }
     }
 }
