1514807dd3c95260d620ac2e347036b97a606f19
SHMONIN <SHMONIN@CHPU-TEH1>
2021-02-25 15:48:56 +0300

    fixed task2

--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -5,17 +5,19 @@ import java.util.Random;
 public class Task2 {
     public static void main(String[] args) {
         Random random = new Random();
-        int min = 0, max = 0, sum0 = 0, count0 = 0;
+        int min = 10000, max = 0, sum0 = 0, count0 = 0;
         int[] array = new int[100];
-        for (int i = 0; i<array.length; i++) {
-            array[i] = random.nextInt(10000);
-            if (array[i] > max)
-                max = array[i];
-            if (array[i] < min)
-                min = array[i];
-            if (array[i] % 10 == 0) {
+        for (int i = 0; i < array.length; i++)
+            array[i] = random.nextInt(min);
+
+        for (int el: array) {
+            if (el > max)
+                max = el;
+            if (el < min)
+                min = el;
+            if (el % 10 == 0) {
                 count0++;
-                sum0 += array[i];
+                sum0 += el;
             }
         }
         System.out.println(max);
