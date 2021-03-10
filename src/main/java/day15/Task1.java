4b1c36f92de0e2203c4337dd5e158be4ffe0293d
SHMONIN <SHMONIN@CHPU-TEH1>
2021-03-10 13:01:46 +0300

    Completed day15 tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,38 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("src/main/resources/shoes.csv");
+        csvToTxtMissingShoes(file);
+    }
+
+    public static void csvToTxtMissingShoes(File file) {
+        List<String> shoes = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext())
+                shoes.add(scanner.nextLine());
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл shoes.csv не найден");
+        }
+
+        try {
+            PrintWriter printWriter = new PrintWriter("src/main/resources/missing_shoes.txt");
+            for (String string : shoes)
+                if (Integer.parseInt(string.split(";")[2]) == 0)
+                    printWriter.println(string.replace(";",", "));
+            printWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл missing_shoes.txt не найден");
+        }
 
     }
 }
