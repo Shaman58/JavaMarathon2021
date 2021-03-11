f5d890c5eeab89e185105758c7f44a48e06ba755
SHMONIN <SHMONIN@CHPU-TEH1>
2021-03-11 08:05:07 +0300

    Fixed day15 tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -3,8 +3,6 @@ package day15;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Task1 {
@@ -14,21 +12,15 @@ public class Task1 {
     }
 
     public static void csvToTxtMissingShoes(File file) {
-        List<String> shoes = new ArrayList<>();
-
-        try {
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNext())
-                shoes.add(scanner.nextLine());
-        } catch (FileNotFoundException e) {
-            System.out.println("Файл shoes.csv не найден");
-        }
-
+        String stringShoes;
         try {
             PrintWriter printWriter = new PrintWriter("src/main/resources/missing_shoes.txt");
-            for (String string : shoes)
-                if (Integer.parseInt(string.split(";")[2]) == 0)
-                    printWriter.println(string.replace(";",", "));
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                stringShoes = scanner.nextLine();
+                if (Integer.parseInt(stringShoes.split(";")[2]) == 0)
+                    printWriter.println(stringShoes.replace(";", ", "));
+            }
             printWriter.close();
         } catch (FileNotFoundException e) {
             System.out.println("Файл missing_shoes.txt не найден");
