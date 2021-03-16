9245a8e4c9605acbb01d596d682ab0d0c4b02b75
SHMONIN <SHMONIN@CHPU-TEH1>
2021-03-16 19:24:26 +0300

    Battleship started

---
+++ src/main/java/SeaBattle/Ship.java
@@ -0,0 +1,21 @@
+package SeaBattle;
+
+import java.util.List;
+
+public class Ship {
+    private int rank;
+    private Position[] position;
+    List<Position> shipArea;
+
+    public void setShip(int x, int y) {
+
+    }
+
+    public void setShip(int x, int y, int rank, int direction) {
+
+    }
+
+    private void fillShipArea() {
+
+    }
+}
